ef843b6b7006819e5805edaf515fc69a75abe207
GMI914 <giorgisaralidze99@gmail.com>
2021-05-01 13:08:17 +0400

    Updated parse_comments.py

--- moodsic/music/management/commands/parse_comments.py
+++ moodsic/music/management/commands/parse_comments.py
@@ -1,8 +1,23 @@
+import requests
 from django.core.management import BaseCommand
 from typing import Any
 
 
+def send_request(URL, params):
+    r = requests.get(URL + "?", params=params)
+    return r.text
+
+
 class Command(BaseCommand):
+    API_KEY = 'AIzaSyDxD2mq5UYvVF-b0vFrqmAradcS3jwhhSs'
+    YOUTUBE_COMMENT_URL = "https://www.googleapis.com/youtube/v3/commentThreads"
+    PARAMS = {
+        'part': 'snippet,replies',
+        'maxResults': 1,
+        'videoId': 'K1jbDJc4cfQ',
+        'textFormat': 'plainText',
+        'key': API_KEY
+    }
 
     def handle(self, *args: Any, **options: Any) -> None:
-        pass
+        print(send_request(self.YOUTUBE_COMMENT_URL, self.PARAMS))
