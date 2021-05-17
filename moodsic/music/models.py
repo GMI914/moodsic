34b908e35c98ad280def742f646173ab15ec65f3
GMI914 <giorgisaralidze99@gmail.com>
2021-05-17 10:08:35 +0400

    Added New Music Models

--- moodsic/music/models.py
+++ moodsic/music/models.py
@@ -1,19 +1,34 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class VideoTags(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=255, null=True, blank=True)
 
 
-# სახელი
-# ვიდეოე id
-# like
-# dislike
-# ნახვების რაოდენობა
-# არხის id
-# ვიდეოს სიგრძე
-# აღწერილობა
-# ტეგები
+class VideoMood(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=255, null=True, blank=True)
 
-# კომენტარების დამუშავებული რეიტინგი
 
-# ხასიათი
-# ჟანრი
+class VideoGenre(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=255, null=True, blank=True)
+
+
+class Music(models.Model):
+    video_id = models.CharField(verbose_name=_('Video Id'), max_length=11, null=False, blank=False)
+    channel_id = models.CharField(verbose_name=_('Channel Id'), max_length=255, null=False, blank=False)
+    title = models.CharField(verbose_name=_('Title'), max_length=511, null=True, blank=True)
+
+    like = models.PositiveIntegerField(verbose_name=_('Like Count'), default=0)
+    dislike = models.PositiveIntegerField(verbose_name=_('Dislike Count'), default=0)
+    views = models.PositiveIntegerField(verbose_name=_('View Count'), default=0)
+    video_length = models.PositiveIntegerField(verbose_name=_('Video Length'))
+
+    description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
+    tags = models.ManyToManyField(verbose_name=_('Tags'), to='music.VideoTags', related_name='videos')
+
+    custom_rating = models.DecimalField(verbose_name=_('Custom Rating'), null=True, blank=True, max_digits=6,
+                                        decimal_places=3)
+
+    mood = models.ManyToManyField(verbose_name=_('Mood'), to='music.VideoMood', related_name='videos')
+    genre = models.ManyToManyField(verbose_name=_('Genre'), to='music.VideoGenre', related_name='videos')
