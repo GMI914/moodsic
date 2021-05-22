d0e784b2349ece25a65e17997c8e4823a16988c7
nika <saganeli.nika@gmail.com>
2021-05-22 15:23:16 +0400

    sending data from our database to the recombee

---
+++ moodsic/music/management/commands/recombee_sendItem.py
@@ -0,0 +1,30 @@
+from typing import Any
+from django.core.management import BaseCommand
+
+from recombee_api_client.api_client import RecombeeClient
+from recombee_api_client.exceptions import APIException
+from recombee_api_client.api_requests import *
+
+from music.models import Music
+
+client = RecombeeClient('moodsic-dev', 'Pb4MhOK6751HmdEGmvISdFJqXjLDWEtVkyb2AIY4Cn1EL3vQWy9V0B236OGEj8iy')
+
+class Command(BaseCommand):
+
+    def handle(self, *args: Any, **options: Any) -> None:
+        musics = Music.objects.all()
+        ratio = 0
+        
+        for music in musics:
+            if music.dislike == 0:
+                 ratio = 0
+            else:
+                ratio = music.like / music.dislike 
+            client.send(SetItemValues(str(music.video_id),
+            {"mood" : "happy",
+            "length" : music.video_length,
+            "viewCount" : music.views,
+            "likeDislikeRatio" : ratio,
+            "title" : music.title,
+            "tags"  : [tag.title for tag in music.tags.all()]},
+            cascade_create=True))
