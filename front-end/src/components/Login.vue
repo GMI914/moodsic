d186ddd2e55a746faca7cd15552b3642b3f1d9a2
Username-ika <56093540+Username-ika@users.noreply.github.com>
2021-06-22 01:04:39 +0400

    Added Ricardo

---
+++ front-end/src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <legend>Login</legend>
+  <p>email</p>
+  <input type="text" name="email" />
+  <p>password</p>
+  <input type="password" name="password" />
+  <input type="submit" value="Proceed!" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+input[type="text"],
+input[type="password"] {
+  display: block;
+  padding: 4px;
+  box-sizing: border-box;
+  border: 2px solid #c3073f;
+  background-color: #254f64;
+  border-radius: 4px;
+  font-size: 21px;
+  font-family: monospace, "Lucida Console";
+  color: #ffff9b;
+  width: 55%;
+  margin: 3px auto 9px;
+}
+
+input[type="submit"] {
+  background-color: #c3073f;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin: 20px auto 0px;
+  width: 40%;
+}
+
+p {
+  font-family: monospace, "Lucida Console";
+  font-style: oblique;
+  color: #05d9e8;
+  font-size: 0.8em;
+  margin: 5px 0px 0px 10px;
+  text-align: center;
+}
+
+legend {
+  font-family: monospace, "Lucida Console";
+  font-style: oblique;
+  font-size: 2em;
+  font-weight: 300;
+  line-height: 0.5em;
+  text-align: center;
+  color: #4dc3fa;
+  text-shadow: 2px 2px red;
+  margin-bottom: 20px;
+}
+</style>
