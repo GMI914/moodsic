acaf8ab99d3201d21c93f7789b38aac84e72c22d
nika <saganeli.nika@gmail.com>
2021-06-23 22:04:15 +0400

    added paths to user

--- moodsic/user/urls.py
+++ moodsic/user/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import routers
+from user.views import UserView
+
+
+router = routers.SimpleRouter()
+router.register(r'user', UserView, basename='user')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('getToken/', ObtainAuthToken.as_view()),
 ]
